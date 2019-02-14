e496f574bca80244f729637eb31b0c723722b2af
JOSUE OROZCO <mac@MacBook-2.local>
2019-02-14 15:59:15 -0600

    Proceso

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -173,9 +173,11 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
+                                <center>
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                   {{ __('Registrar') }}
                                 </button>
+                                </center> 
                             </div>
                         </div>
                     </form>
